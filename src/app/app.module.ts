58c6844c0a2713cc2f804c3d8dd842bff35fce6e
DESKTOP-L6P1MUL\DangTuan <bachdangtuan93@gmail.com>
2022-10-05 20:07:46 +0700

    Update DataBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './layout/homepage/homepage.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './layout/homepage/homepage.component';
 import {SidebarComponent} from "./component/sidebar/sidebar.component";
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { ContentComponent } from './component/content/content.component';
-import { TypePageOneComponent } from './component/page/typescript/type-page-one/type-page-one.component';
-import { TypePage2Component } from './component/page/typescript/type-page2/type-page2.component';
-import { TypePage3Component } from './component/page/typescript/type-page3/type-page3.component';
-import {AppRoutingModule, RoutingComponent } from "./app-routing/app-routing.module";
-import { AngularPage1Component } from './component/page/angular/angular-page1/angular-page1.component';
-import { AngularPage2Component } from './component/page/angular/angular-page2/angular-page2.component';
-import { AngularPage3Component } from './component/page/angular/angular-page3/angular-page3.component';
-import { AngularPage4Component } from './component/page/angular/angular-page4/angular-page4.component';
-import { AngularPage5Component } from './component/page/angular/angular-page5/angular-page5.component';
-import { AngularPage6Component } from './component/page/angular/angular-page6/angular-page6.component';
-import { HomeContentComponent } from './component/page/trangchu/home-content/home-content.component';
+import {NavbarComponent} from './component/navbar/navbar.component';
+import {ContentComponent} from './component/content/content.component';
+import {TypePageOneComponent} from './component/page/typescript/type-page-one/type-page-one.component';
+import {TypePage2Component} from './component/page/typescript/type-page2/type-page2.component';
+import {TypePage3Component} from './component/page/typescript/type-page3/type-page3.component';
+import {AppRoutingModule, RoutingComponent} from "./app-routing/app-routing.module";
+import {AngularPage1Component} from './component/page/angular/angular-page1/angular-page1.component';
+import {AngularPage2Component} from './component/page/angular/angular-page2/angular-page2.component';
+import {AngularPage3Component} from './component/page/angular/angular-page3/angular-page3.component';
+import {AngularPage4Component} from './component/page/angular/angular-page4/angular-page4.component';
+import {AngularPage5Component} from './component/page/angular/angular-page5/angular-page5.component';
+import {AngularPage6Component} from './component/page/angular/angular-page6/angular-page6.component';
+import {HomeContentComponent} from './component/page/trangchu/home-content/home-content.component';
 
 
 @NgModule({
@@ -39,10 +39,11 @@ import { HomeContentComponent } from './component/page/trangchu/home-content/hom
     HomeContentComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/component/page/angular/angular-page3/angular-page3.component.ts
+++ src/app/component/page/angular/angular-page3/angular-page3.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, FormArray, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-angular-page3',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./angular-page3.component.css']
 })
 export class AngularPage3Component implements OnInit {
+  public formInfor: any; // Khai báo 1 biến public formInfor với kiểu dữ liệu bất kỳ
+  public inforCustomer: any = {}// Kiểu dữ là object để hứng dữ liệu khi lấy từ form Input
+  public submitted = false;
+  public date = new Date()
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.formInfor = FormGroup // Khởi tạo biến là FormGroup
   }
 
+  ngOnInit(): void {
+
+    console.log('inforCustomer',this.inforCustomer)
+    this.formInfor = this.fb.group({
+      name: new FormControl('', [Validators.required]
+      ),
+      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+    })
+
+  }
+  get f() { return this.formInfor.controls; }
+
+  confirmInfor() {
+    this.submitted = true;
+    if (!this.formInfor.invalid) {
+      // console.log(this.formInfor.value)
+      this.inforCustomer = this.formInfor.value
+    }
+  }
 }
