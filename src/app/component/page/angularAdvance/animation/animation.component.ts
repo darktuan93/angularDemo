d1df77ee55a54792e46698cd8fed93838ce1d00c
DESKTOP-L6P1MUL\DangTuan <bachdangtuan93@gmail.com>
2022-10-08 01:40:55 +0700

    no message

--- src/app/component/page/angularAdvance/animation/animation.component.ts
+++ src/app/component/page/angularAdvance/animation/animation.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {trigger, transition, animate, style, state} from '@angular/animations';
+import {trigger, transition, animate, style, keyframes, state} from '@angular/animations';
 
 @Component({
   selector: 'app-animation',
@@ -8,22 +8,55 @@ import {trigger, transition, animate, style, state} from '@angular/animations';
   animations: [
     trigger('animationTest', [
       transition(':enter', [
-        style({transform: 'scale(0.4)', opacity: 0.4,background:'red'}),
+        style({transform: 'scale(0.4)', opacity: 0.4, background: 'red'}),
         animate('1000ms ease-in', style({transform: 'scale(1)', 'opacity': 0.7}))
       ]),
+    ]),
+    trigger('animationTest2', [
+      state('state1', style({
+        color: 'black',
+        transform: 'scale(1.5)'
+      })),
+      state('state2', style({
+        color: 'black',
+        transform: 'scale(0.5)'
+      })),
+      state('state3', style({
+        color: 'blue',
+        transform: 'scale(1.1)'
+      })),
+      transition('state2=>state1', animate('2000ms')),
+      transition('state1=>state2', animate('2000ms')),
+      transition('state2=>state3', animate('5s', keyframes([
+        style({transform: 'translateX(0%)'}),
+        style({transform: 'translateY(100%)'}),
+        style({transform: 'translateX(0%)'})
+      ])),)
     ])
   ]
 })
 export class AnimationComponent implements OnInit {
-  show: boolean = true;
+  show: boolean = true
+  public animationState: any
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.animationState = 'state2'
+
   }
 
   toggle($event: any) {
     this.show = !this.show;
+  }
+
+  toggle2($event: MouseEvent) {
+    this.animationState = 'state1'
+  }
+
+  checkValue() {
+    console.log('tét')
+    this.animationState = 'state3'
   }
 }
